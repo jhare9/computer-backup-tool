552df184fa60d127b2482b991c4914c0b5215083
jonathon hare <jonathon.hare@eagles.usm.edu>
2016-01-16 21:21:09 -0600

    Initial Commit

---
+++ computer backup tool/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  computer backup tool
+//
+//  Created by jonathon hare on 1/16/16.
+//  Copyright © 2016 jonathon hare. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
